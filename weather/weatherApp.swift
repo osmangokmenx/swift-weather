7864a6e06877605ac44de440f01948d01c6e441a
osmangokmenx <52015838+osmangokmenx@users.noreply.github.com>
2022-09-21 13:25:08 +0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Osman Gökmen on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
